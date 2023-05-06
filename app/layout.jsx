c6b737db05d18ece4b1f4b5017806b0a0bdc3a44
NickCoder123 <nikunjmehra935@gmail.com>
2023-05-06 11:06:52 +0530

    Created Home Layout and Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
   title: "Promptopia",
   description: "Discover and share AI Prompts"
@@ -14,6 +17,7 @@ const RootLayout = ({ children }) => {
         </div>
 
         <main className="app">
+          <Nav />
           {children}
         </main>
       </body>
